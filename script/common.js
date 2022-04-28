3fd6c73d2aac7feae0df13c0f3c6f588787f5047
amazingHanim <chee_up_8282@naver.com>
2022-04-28 16:46:57 +0900

    d
    d

--- script/common.js
+++ script/common.js
@@ -1,6 +1,7 @@
 $(function(){
     function mainMenu() {
-        var $hdrWrap = $('.hdrWrap'),
+        var $body = $('body'),
+            $hdrWrap = $body.find(".hdrWrap"),
             $mainMenuList = $hdrWrap.find(".mainMenu"),
             $mainMenu = $mainMenuList.children(),
             $subMenuList = $mainMenu.find(".subMenu"),
@@ -12,20 +13,22 @@ $(function(){
 
         
         $hamBtn.on("click",function (){
-            $hdrWrap.toggleClass("on")
-
+            $body.toggleClass("on");
         });
 
         $mainMenu.on("mouseenter", function () {
             num = $(this).index();
             $subMenuList.hide()
-            $(this).find($subMenuList).slideDown(300)
+            $(this).find($subMenuList).slideDown(300);
 
         });
         $hdrWrap.on("mouseleave", function () {
             $subMenuList.hide()
-
         });
+        $(window).on("resize",function(){
+            $body.removeClass("on");
+            $subMenuList.hide();
+        })
 
     }
     mainMenu();
